bd3a59db61901842cb4ea8f537fe41f29a3ade62
Awan Berlian <berlianawan7@gmail.com>
2025-01-11 14:04:37 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 5000;
+const port = 8080;
 
 // API Key untuk layanan Virtual SIM
 const API_KEY = '8UAkynwz2f6ION1SVGCbBTFhMDajQr'; // Ganti dengan API Key yang diberikan
